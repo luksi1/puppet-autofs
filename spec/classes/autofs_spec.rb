ce9b27047adb6fde58448a5d62babd62a1c3ae55
luksi1 <luke.simmons@vgregion.se>
2020-03-31 11:33:35 +0200

    Linting

--- spec/classes/autofs_spec.rb
+++ spec/classes/autofs_spec.rb
@@ -4,6 +4,7 @@ describe 'autofs' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+
       it { is_expected.to compile }
       it { is_expected.to contain_package('autofs') }
       it { is_expected.to contain_service('autofs') }
